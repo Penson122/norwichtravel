7cd686c35717c9620360e30ff3b086424ba4e3a5
jpenson <jpenson@uea.ac.uk>
2017-12-06 21:01:46 +0000

    implemented train page

--- routes/Bus.js
+++ routes/Bus.js
@@ -4,6 +4,8 @@ import { ScrollView, StyleSheet, Platform } from 'react-native';
 import Search from '../components/Search';
 import SearchResults from '../components/SearchResults';
 
+import { getAutoCompleteList, getLatLng } from '../util';
+
 import { NORWICH_API } from '../constants.js';
 
 const styles = StyleSheet.create({
@@ -19,18 +21,54 @@ class Bus extends Component {
     super(props);
     this.state = {
       results: [],
+      originText: '',
+      hasSelected: false,
+      stops: [],
       placeholder: {
         origin: 'Search for road...'
       },
+      originAutoComplete: [],
       options: {
         destination: false
       }
     };
     this.getFromAPI = this.getFromAPI.bind(this);
     this.getTimeTable = this.getTimeTable.bind(this);
-    this.nearHandler = this.nearHandler.bind(this);
     this.searchHandler = this.searchHandler.bind(this);
+    this.originChange = this.originChange.bind(this);
+    this.onOriginSelect = this.onOriginSelect.bind(this);
   }
+
+  originChange (text) {
+    this.setState({ originText: text });
+    if (text.length > 3) {
+      const url = NORWICH_API + '/autocomplete/bus/' + text;
+      getAutoCompleteList(url).then(response => {
+        const descriptions = response.map((r, i) => ({ description: r.description, key: i }));
+        this.setState({ originAutoComplete: descriptions });
+      });
+    }
+  };
+
+  onOriginSelect (text) {
+    this.setState({ originText: text, originAutoComplete: [] });
+    if (!this.state.hasSelected) {
+      getLatLng(text).then(geocode => {
+        const url = NORWICH_API + '/bus/near/' + geocode.lat + '/' + geocode.lng;
+        this.getFromAPI(url).then(response => {
+          const stops = response.stops.map((s, i) => ({ description: s.name, key: i }));
+          this.setState({
+            originAutoComplete: stops,
+            stops: response.stops,
+            hasSelected: true
+          });
+        });
+      });
+    }
+    if (this.state.hasSelected) {
+      this.setState({ hasSelected: false });
+    }
+  };
 
   getFromAPI (url) {
     return fetch(url)
@@ -49,15 +87,10 @@ class Bus extends Component {
     return this.getFromAPI(url);
   };
 
-  nearHandler (lat, lng) {
-    const url = NORWICH_API + '/bus/near/' + lat + '/' + lng;
-    return this.getFromAPI(url);
-  };
-
   searchHandler (origin) {
     if (origin !== undefined) {
-      console.log(origin.atcocode);
-      this.getTimeTable(origin.atcocode).then(response => {
+      const originStop = this.state.stops.find(s => s.name === origin);
+      this.getTimeTable(originStop.atcocode).then(response => {
         this.setState({ results: response });
       });
     }
@@ -67,12 +100,20 @@ class Bus extends Component {
       <ScrollView style={styles.container}>
         <Search
           submitHandler={this.searchHandler}
-          nearHandler={this.nearHandler}
+          originText={this.state.originText}
+          onOriginChange={this.originChange}
+          originAutoComplete={this.state.originAutoComplete}
+          onOriginSelect={this.onOriginSelect}
           options={this.state.options}
           placeholder={this.state.placeholder}
-          defaults={{}}
+          defaults={{ origin: true }}
         />
-        <SearchResults results={this.state.results} style={{ paddingTop: '3%' }} />
+        <SearchResults
+          results={this.state.results}
+          type='bus'
+          lineKey='line'
+          destinationKey='direction'
+        />
       </ScrollView>
     );
   };
