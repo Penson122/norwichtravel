f9c8eb4ba3b9cad87c302a97cf65400c8750c98b
Christopher Irvine <christopher@irvinegroup.eu>
2017-12-07 16:02:33 +0000

    Added centering and background colour, dark green

--- routes/Home.js
+++ routes/Home.js
@@ -5,20 +5,30 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Platform.OS === 'ios' ? '5%' : 0, // fix for padding in expo apps on android
     marginRight: '1%',
-    marginLeft: '1%'
+    marginLeft: '1%',
+    backgroundColor: '#3ea01b'
+  },
+  welcomeTextBox: {
+    textAlign: 'centre',
+  },
+  welcome: {
+    justifyContent:'centre',
+    fontsize: '2em'
   },
 });
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text style={{ justifyContent:'centre', fontsize: '2em' }}>Welcome to Norwich Travel!</Text>
-      <Text>Please use the navigation bar down the bottom of your screen for:
-        <FlatList
-          data={[ { key: 'Bus Times' }, { key: 'Train Times' }, { key: 'Taxi Numbers' }, { key: 'FLight Times' } ]}
-          renderItem={({ item }) => <Text>{item.key}</Text>}
-        />
-      </Text>
+      <View style={styles.welcomeTextBox}>
+        <Text style={styles.welcome}>Welcome to Norwich Travel!</Text>
+        <Text>Please use the navigation bar down the bottom of your screen for:
+          <FlatList
+            data={[ { key: 'Bus Times' }, { key: 'Train Times' }, { key: 'Taxi Numbers' }, { key: 'FLight Times' } ]}
+            renderItem={({ item }) => <Text>{item.key}</Text>}
+          />
+        </Text>
+      </View>
     </View>
   );
 };
